931fd7647ba98b0b5495f77a26a8e51f12bf425f
dbstj589 <lysfox01@gmail.com>
2025-11-14 19:42:59 +0900

    add : create project

---
+++ src/test/java/cms_back/CmsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cms_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
